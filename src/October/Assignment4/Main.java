835ecc6c5a9caea4849c93fbd678ef1a1a72591a
andrewrusso <adrswimfast@gmail.com>
2016-10-20 11:49:15 -0400

    Assignment 4 completed

--- src/October/Assignment4/Main.java
+++ src/October/Assignment4/Main.java
@@ -1,5 +1,6 @@
 package October.Assignment4;
 import java.util.Scanner;
+import java.lang.String;
 public class Main
 {
     public static void main(String[] Args){
@@ -7,15 +8,45 @@ public class Main
         System.out.println("Please enter a tweet:");
         String tweet = scan.nextLine();
         int length = tweet.length();
+        int atcount = 0;
+        int hashcount = 0;
+        boolean rt = true;
         if(length > 140)
         {
             int excess = length - 140;
             System.out.println("Excess Characters: " + excess);
         }
-        else if(length <= 140)
-        {
+        else if(length <= 140) {
+            if (tweet.indexOf("RT:") >= 0 || tweet.indexOf("rt:") >= 0 || tweet.indexOf("rT:") >= 0 || tweet.indexOf("Rt:") >= 0) {
+                rt = true;
+            } else if (tweet.indexOf("RT:") <=0 || tweet.indexOf("rt:") <=0 || tweet.indexOf("rT:") <=0 || tweet.indexOf("Rt:") <=0)
+            {
+                rt = false;
+            }
+            for( int i=0; i<(tweet.length() -1); i ++ )
+            {
+                if( tweet.charAt(i) == '@' && tweet.charAt(i+1) != ' ' && tweet.charAt(i+1) != '\t')
+                {
+                    atcount++;
+                }
+            }
+            for( int i=0; i<(tweet.length()-1); i ++) {
+                if( tweet.charAt(i) == '#' && tweet.charAt(i+1) != ' ' && tweet.charAt(i+1) != '\t')
+                {
+                    hashcount++;
+                }
+            }
+            System.out.println("Length Correct");
+            System.out.println("Number of Mentions: "+atcount);
+            System.out.println("Number of Hashtags: "+hashcount);
+            if(rt == true)
+            {
+                System.out.println("The input was a retweet.");
+            }
+            if(rt == false)
+            {
+                System.out.println("The input was not a retweet.");
+            }
         }
-
     }
-
 }
