e2e225ab35361c5ee162642ef80e7e88bb15592b
andrewrusso <adrswimfast@gmail.com>
2016-10-24 10:16:11 -0400

    lessons 22-24 completed

--- src/October/Lesson24/Lesson_24_Activity_Three.java
+++ src/October/Lesson24/Lesson_24_Activity_Three.java
@@ -1,7 +1,23 @@
 package October.Lesson24;
-
-/**
- * Created by andrewrusso on 10/24/16.
- */
+import java.util.Scanner;
 public class Lesson_24_Activity_Three {
+    public static void main(String [] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number between 0 and 100:");
+        int num = scan.nextInt();
+        int print = 0;
+        if( num >=0 && num <= 100)
+        {
+            for (int i = num; i <= 100; i++)
+            {
+                System.out.print(i + (print % 20 == 0 ? "\n" : " "));
+            }
+        }
+        else
+        {
+            System.out.println("error");
+        }
+
+    }
 }
--- src/October/Lesson24/Lesson_24_Activity_Two.java
+++ src/October/Lesson24/Lesson_24_Activity_Two.java
@@ -4,5 +4,11 @@ package October.Lesson24;
  * Created by andrewrusso on 10/24/16.
  */
 public class Lesson_24_Activity_Two {
-    public static void main(String [] Args);
+    public static void main(String [] args)
+    {
+        for( int i = 2; i <= 50; i+=2)
+        {
+            System.out.println(i);
+        }
+    }
 }
