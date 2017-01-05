8be9b76bcdc215a471eabbf31abfbdd473bc372b
andrewrusso <adrswimfast@gmail.com>
2017-01-05 10:56:20 -0500

    Final commit, all work is now moved to project APCS2

--- src/test.java
+++ src/test.java
@@ -2,12 +2,17 @@
  * Created by andrewrusso on 12/2/16.
  */
 public class test {
-    public static void main (String [] args)
+    public static void mystery(String wo) {
+        if (wo.length() > 0)
+            mystery( wo.substring( wo.length() - 1));
+        System.out.println(wo);
+    }
+
+    public static void main (String[]args)
     {
-        int n = -12;
-        //while( n <= -20  && n => -11)
-        {
-            n = (int)(Math.random() * 20) - 10;
-        }
+        String q = "adjective";
+        String r = "stinky";
+
+        System.out.println( q.charAt( r.indexOf ('t')));
     }
 }
